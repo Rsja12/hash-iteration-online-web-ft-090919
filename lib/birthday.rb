46de1284783ecfd37e0445481861765ff9e47d57
Alfonso Socarras <asocarras10@gmail.com>
2019-09-14 02:16:21 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,9 +12,19 @@ end
 
 
 def age_appropriate_birthday(birthday_kids)
+  message = ""
   birthday_kids.each do |kids_name, age|
     if age <= 12 
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+      message = "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
    end 
+   puts message
 end
+
+
+
+
+
+
+
+
